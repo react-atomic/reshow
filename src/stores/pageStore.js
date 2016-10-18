5ee24c9151f995ea7455c06f2548c59753efce80
Hill <hill@kimo.com>
2016-10-18 09:41:25 +0000

    add back update url

--- src/stores/pageStore.js
+++ src/stores/pageStore.js
@@ -6,23 +6,28 @@ import dispatchServer from '../actions/dispatcher';
 
 class PageStore extends ReduceStore
 {
+    getInitialState()
+    {
+        return Immutable.Map();
+    }
 
-  getInitialState()
-  {
-      return Immutable.Map();
-  }
+    updateUrl(url)
+    {
+        history.pushState('','',url);
+    }
 
-  reduce (state, action)
-  {
-        switch (action.type)
-        {
+    reduce (state, action)
+    {
+        if (action.url) {
+            this.updateUrl(action.url);
+        }
+        switch (action.type) {
             case 'config/set':
                 return state.merge(action.params);
             default:
                 return state;
         }
-  }
-
+    }
 }
 
 // Export a singleton instance of the store, could do this some other way if
