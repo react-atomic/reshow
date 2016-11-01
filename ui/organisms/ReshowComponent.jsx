62f0671849a42c01d3735d7b6510910d1538dfcc
Hill <hill@kimo.com>
2016-11-01 12:56:43 +0000

    improve ReshowComponent

--- ui/organisms/ReshowComponent.jsx
+++ ui/organisms/ReshowComponent.jsx
@@ -17,8 +17,14 @@ class ReshowComponent extends Component
         if (global.path !== pageState.get('themePath')) {
             return prevState;
         }
-        const data = pageState.get('data').toJS();
-        const i18n = pageState.get('I18N').toJS();
+        let data = pageState.get('data');
+        let i18n = pageState.get('I18N');
+        if (data && data.toJS) {
+            data = data.toJS();
+        }
+        if (i18n && i18n.toJS) {
+            i18n = i18n.toJS();
+        }
         return {
             data: data,
             I18N: i18n
