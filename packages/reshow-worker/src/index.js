404693021ce9ccf816a11f5176eecd1908bc2e6e
Hill <hill@kimo.com>
2018-04-09 10:30:02 +0000

    update for react 16.3

--- packages/reshow-worker/src/index.js
+++ packages/reshow-worker/src/index.js
@@ -1,4 +1,5 @@
 import {initAjaxWorkerEvent} from 'organism-react-ajax';
+import {getDefault} from 'get-object-value';
 
 const initWorker = () =>
 {
@@ -6,6 +7,7 @@ const initWorker = () =>
         if (window.Worker) {
             import('worker-loader!organism-react-ajax/build/es/src/worker').
                 then( workerObject => {
+                    workerObject = getDefault(workerObject);
                     if (workerObject) {
                         const objWorker = new workerObject();
                         initAjaxWorkerEvent(objWorker);
