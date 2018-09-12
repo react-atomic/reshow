c56367833e0d30e289836bff93ef6102221fd6af
Hill <hill@kimo.com>
2018-09-12 07:32:26 +0000

    Add objectWithoutProperties

---
+++ packages/reshow-runtime/src/helpers/objectWithoutProperties.js
@@ -0,0 +1,11 @@
+const objectWithoutProperties = (obj, keys) => {
+  var target = {};
+  for (var i in obj) {
+    if (keys.indexOf(i) >= 0) continue;
+    if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
+    target[i] = obj[i];
+  }
+  return target;
+}
+
+export default objectWithoutProperties
