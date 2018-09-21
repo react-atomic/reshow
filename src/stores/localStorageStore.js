ad5eca5a03b3d5865747fc3dcfdeff7c27d6fcee
Hill <hill@kimo.com>
2018-09-21 10:45:25 +0800

    update storage package

--- src/stores/localStorageStore.js
+++ src/stores/localStorageStore.js
@@ -1,81 +1,29 @@
-import {ReduceStore} from 'reshow-flux'
-import get from 'get-object-value'
-import {localStorage} from 'get-storage'
+import {ReduceStore} from 'reshow-flux';
+import get from 'get-object-value';
+import {localStorage, Storage} from 'get-storage';
 
-import storageDispatcher from '../storageDispatcher'
+import storageDispatcher from '../storageDispatcher';
 
-const keys = Object.keys
+class LocalStorageStore extends ReduceStore {
+  getInitialState() {
+    return new Storage(localStorage);
+  }
 
-const toInt = d => parseInt(d, 10)
+  updateStorage(state, action) {
+    const params = get(action, ['params']);
+    return state.merge(params);
+  }
 
-class Storage
-{
-    constructor(_storage)
-    {
-        this._storage = _storage
+  reduce(state, action) {
+    switch (action.type) {
+      case 'local':
+        return this.updateStorage(state, action);
+      default:
+        return state;
     }
-
-    set(k, v)
-    {
-        const _v = JSON.stringify(v)
-        const vLen = _v.length
-        const s = vLen+','+_v
-        this._storage(k)(s)
-    }
-
-    get(k)
-    {
-        const s = this._storage(k)()
-        if (!s) {return undefined}
-        const iStar = s.indexOf(',')
-        const vLen = toInt(s.substring(0,iStar))
-        const value = s.substr(iStar+1) 
-        if (vLen === value.length) {
-            return JSON.parse(value)
-        } else {
-            return undefined 
-        }
-    }
+  }
 }
 
-class LocalStorageStore extends ReduceStore
-{
-    getInitialState()
-    {
-        return new Storage(localStorage)
-    }
+export default new LocalStorageStore(storageDispatcher);
 
-    updateStorage(state, action)
-    {
-        const params = get(action, ['params'])
-        if (!params || 'object' !== typeof params) {
-            return state
-        }
-        const pKeys = keys(params)
-        if (!pKeys || !pKeys.length) {
-            return state
-        }
-        pKeys.forEach(
-            key => state.set(key, params[key])
-        )
-        // force udpate
-        return this.getInitialState()
-    }
-
-    reduce (state, action)
-    {
-        switch(action.type) {
-            case 'local':
-                return this.updateStorage(state, action)
-            default:
-                return state
-        }
-    }
-}
-
-export default new LocalStorageStore(storageDispatcher)
-
-export {
-    LocalStorageStore,
-    Storage
-}
+export {LocalStorageStore};
--- src/stores/sessionStorageStore.js
+++ src/stores/sessionStorageStore.js
@@ -1,24 +1,21 @@
-import {sessionStorage} from 'get-storage'
+import {sessionStorage, Storage} from 'get-storage';
 
-import storageDispatcher from '../storageDispatcher'
-import {LocalStorageStore, Storage} from './localStorageStore'
+import {LocalStorageStore} from './localStorageStore';
+import storageDispatcher from '../storageDispatcher';
 
-class SessionStorageStore extends LocalStorageStore
-{
-    getInitialState()
-    {
-        return new Storage(sessionStorage)
+class SessionStorageStore extends LocalStorageStore {
+  getInitialState() {
+    return new Storage(sessionStorage);
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case 'session':
+        return this.updateStorage(state, action);
+      default:
+        return state;
     }
-
-    reduce (state, action)
-    {
-        switch(action.type) {
-            case 'session':
-                return this.updateStorage(state, action)
-            default:
-                return state
-        }
-    }
+  }
 }
 
-export default new SessionStorageStore(storageDispatcher)
+export default new SessionStorageStore(storageDispatcher);
