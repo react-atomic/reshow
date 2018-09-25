8931d32f484dbda1ec893688c91293806b10a697
Hill <hill@kimo.com>
2018-09-25 18:19:09 +0800

    Add toConsumableArray

---
+++ packages/reshow-runtime/src/helpers/toConsumableArray.js
@@ -0,0 +1,13 @@
+
+const toConsumableArray = arr => {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+}
+
+export default toConsumableArray;
