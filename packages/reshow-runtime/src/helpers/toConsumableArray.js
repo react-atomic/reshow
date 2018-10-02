91a828bc2b70b08b785ae720131e6a439a86ae27
Hill <hill@kimo.com>
2018-10-02 16:13:39 +0800

    better support for array.from

--- packages/reshow-runtime/src/helpers/toConsumableArray.js
+++ packages/reshow-runtime/src/helpers/toConsumableArray.js
@@ -1,6 +1,5 @@
-
 const toConsumableArray = arr => {
-  if (Array.isArray(arr)) {
+  if (Array.isArray(arr) || !Array.from) {
     for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
       arr2[i] = arr[i];
     }
@@ -8,6 +7,6 @@ const toConsumableArray = arr => {
   } else {
     return Array.from(arr);
   }
-}
+};
 
 export default toConsumableArray;
