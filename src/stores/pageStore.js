502c38eb1edcb850b064d0ecde51856601b9887d
Hill <hill@kimo.com>
2018-10-22 23:14:31 +0800

    Add getThemePath

--- src/stores/pageStore.js
+++ src/stores/pageStore.js
@@ -3,25 +3,32 @@ import dispatcher from '../dispatcher';
 
 const keys = Object.keys;
 
-class PageStore extends ReduceStore
-{
-    reduce (state, action)
-    {
-        switch (action.type) {
-            case 'config/set':
-                return state.merge(action.params);
-            case 'config/reset':
-                return state.clear().merge(action.params);
-            case 'realTime':
-                return state;
-            default:
-                if (keys(action)) {
-                    return state.merge(action);
-                } else {
-                    return state;
-                }
+class PageStore extends ReduceStore {
+  getThemePath() {
+    const state = this.getState();
+    let themePath = state.get('themePath');
+    if (!themePath) {
+      themePath = state.get('defaultThemePath');
+    }
+    return themePath;
+  }
+
+  reduce(state, action) {
+    switch (action.type) {
+      case 'config/set':
+        return state.merge(action.params);
+      case 'config/reset':
+        return state.clear().merge(action.params);
+      case 'realTime':
+        return state;
+      default:
+        if (keys(action)) {
+          return state.merge(action);
+        } else {
+          return state;
         }
     }
+  }
 }
 
 // Export a singleton instance of the store
--- ui/molecules/Reshow.jsx
+++ ui/molecules/Reshow.jsx
@@ -4,121 +4,112 @@ import {AjaxPage} from 'organism-react-ajax';
 import {connect} from 'reshow-flux';
 import {localStorage as lStore} from 'get-storage';
 
-import {
-    dispatch,
-    global,
-    pageStore,
-} from '../../src/index';
+import {dispatch, global, pageStore} from '../../src/index';
 
 const isArray = Array.isArray;
 
 let doc;
 let isInit;
 
-const updateCanonicalUrl = (url, props) =>
-{
-    if ( get(props, ['disableCanonical'], () => lStore('disableCanonical')() )) {
-        return;
-    }
-    const loc = doc.location; 
-    const newUrl = url+ loc.search+ loc.hash;
-    if (-1 !== url.indexOf(loc.hostname)) {
-        history.replaceState('', '', newUrl);
-    } else {
-        loc.replace(newUrl);
-    }
+const updateCanonicalUrl = (url, props) => {
+  if (get(props, ['disableCanonical'], () => lStore('disableCanonical')())) {
+    return;
+  }
+  const loc = doc.location;
+  const newUrl = url + loc.search + loc.hash;
+  if (-1 !== url.indexOf(loc.hostname)) {
+    history.replaceState('', '', newUrl);
+  } else {
+    loc.replace(newUrl);
+  }
 };
 
-const update = params => 
-{
-    const realTimeData = get(params, ['--realTimeData--']);
-    const reset = get(params, ['--reset--']);
-    let type;
-    if (realTimeData) {
-        type = 'realTime';
-    } else {
-        type = 'config/'+ ((reset) ? 're' : '')+ 'set';
+const update = params => {
+  const realTimeData = get(params, ['--realTimeData--']);
+  const reset = get(params, ['--reset--']);
+  let type;
+  if (realTimeData) {
+    type = 'realTime';
+  } else {
+    type = 'config/' + (reset ? 're' : '') + 'set';
+  }
+  dispatch({type, params});
+  if (doc) {
+    const htmlTitle = get(params, ['htmlTitle']);
+    if (htmlTitle) {
+      if (isArray(htmlTitle)) {
+        doc.title = get(htmlTitle, [0]);
+      } else {
+        doc.title = htmlTitle;
+      }
     }
-    dispatch({ type, params });
-    if (doc) {
-        const htmlTitle = get(params, ['htmlTitle']);
-        if (htmlTitle) {
-            if (isArray(htmlTitle)) {
-                doc.title = get(htmlTitle, [0]);
-            } else {
-                doc.title = htmlTitle;
-            }
-        }
-        const canonical = get(params, ['data', 'canonical']);
-        if (canonical) {
-            updateCanonicalUrl(canonical, params);
-        }
+    const canonical = get(params, ['data', 'canonical']);
+    if (canonical) {
+      updateCanonicalUrl(canonical, params);
     }
+  }
 };
 
-class Reshow extends PureComponent
-{
-    static getStores()
-    {
-        return [pageStore];
-    }
+class Reshow extends PureComponent {
+  static getStores() {
+    return [pageStore];
+  }
 
-    static calculateState(prevState)
-    {
-        const pageState = pageStore.getState();
-        global.path = pageState.get('themePath');
-        return {
-          themePath: global.path,
-          baseUrl: pageState.get('baseUrl'),
-          staticVersion: pageState.get('staticVersion')
-        }; 
-    }
+  static calculateState(prevState) {
+    const pageState = pageStore.getState();
+    global.path = pageStore.getThemePath();
+    return {
+      themePath: global.path,
+      baseUrl: pageState.get('baseUrl'),
+      staticVersion: pageState.get('staticVersion'),
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        if (isInit) {
-            console.warn('The best practice is avoid multi Reshow Component.');
-            this.stop = true;
-        } else {
-            update(props);
-            this.stop = false;
-            isInit = 1;
-        }
+  constructor(props) {
+    super(props);
+    if (isInit) {
+      console.warn('The best practice is avoid multi Reshow Component.');
+      this.stop = true;
+    } else {
+      update(props);
+      this.stop = false;
+      isInit = 1;
     }
+  }
 
-    componentDidMount()
-    {
-        doc = document;
-        const canonical = doc.querySelector('link[rel="canonical"]');
-        if (-1 !== doc.URL.indexOf('--disableCanonical')) {
-            lStore('disableCanonical')(1);
-        } else if (canonical && canonical.href) {
-            updateCanonicalUrl(canonical.href, this.props);
-        }
+  componentDidMount() {
+    doc = document;
+    const canonical = doc.querySelector('link[rel="canonical"]');
+    if (-1 !== doc.URL.indexOf('--disableCanonical')) {
+      lStore('disableCanonical')(1);
+    } else if (canonical && canonical.href) {
+      updateCanonicalUrl(canonical.href, this.props);
     }
+  }
 
-    render()
-    {
-        if (this.stop) {
-            return null;
-        }
-        const {themes, defaultThemePath, ajax, webSocketUrl} = this.props;
-        const {themePath, baseUrl, staticVersion} = this.state;
-        return (
-            <AjaxPage 
-                callback={ json => update(json) }
-                /*State*/
-                themePath={themePath}
-                baseUrl={baseUrl}
-                staticVersion={staticVersion} /* Keep pure value, let user don't assin etra value for emtpy. */
-                /*Props*/
-                themes={themes}
-                defaultThemePath={defaultThemePath}
-                ajax={ajax}
-                webSocketUrl={webSocketUrl}
-            />
-        );
+  render() {
+    if (this.stop) {
+      return null;
     }
+    const {themes, defaultThemePath, ajax, webSocketUrl} = this.props;
+    const {themePath, baseUrl, staticVersion} = this.state;
+
+    return (
+      <AjaxPage
+        callback={json => update(json)}
+        /*State*/
+        themePath={themePath || defaultThemePath}
+        baseUrl={baseUrl}
+        staticVersion={
+          staticVersion
+        } /* Keep pure value, let user don't assin etra value for emtpy. */
+        /*Props*/
+        themes={themes}
+        ajax={ajax}
+        webSocketUrl={webSocketUrl}
+      />
+    );
+  }
 }
 
 export default connect(Reshow);
