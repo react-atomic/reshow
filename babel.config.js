ca89794b3c878450bb50d486e8d4ab774c0b2dc7
Hill <hill@kimo.com>
2018-10-28 04:09:40 +0800

    update to babel7

---
+++ babel.config.js
@@ -0,0 +1,59 @@
+module.exports = function(api) {
+  api.cache(true); 
+  return {
+    env: {
+      build: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              targets: ['last 2 versions', 'ie >= 8'],
+            },
+          ],
+          '@babel/react',
+        ],
+        plugins: [
+          'transform-react-pure-class-to-function',
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              regenerator: false,
+            },
+          ],
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-transform-react-constant-elements',
+          '@babel/plugin-transform-object-assign',
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-class-properties',
+        ],
+      },
+      es: {
+        presets: [
+          [
+            '@babel/env',
+            {
+              modules: false,
+              targets: ['last 2 versions', 'ie >= 8'],
+            },
+          ],
+          '@babel/react',
+        ],
+        plugins: [
+          'transform-react-pure-class-to-function',
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              regenerator: false,
+              useESModules: true,
+            },
+          ],
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-transform-object-assign',
+          '@babel/plugin-transform-react-constant-elements',
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-class-properties',
+        ],
+      },
+    },
+  };
+};
--- packages/reshow-runtime/README.md
+++ packages/reshow-runtime/README.md
@@ -6,6 +6,11 @@ babel runtime alternative
 * NPM
    * https://www.npmjs.com/package/reshow-runtime
 
+## reshow-runtime only use in Babel 6
+When babel7 runtime already support useESModules, keep reshow-runtime for babel6 
+* https://babeljs.io/docs/en/babel-plugin-transform-runtime#useesmodules
+
+## moduleName
 * options
    * 6.0
       * moduleName
