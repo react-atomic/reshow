a416078d0bbc2b3e07d95fab2e05c8c9a2555359
Hill <hill@kimo.com>
2018-11-27 23:20:11 +0800

    improve get typeof for ie11

--- packages/reshow-runtime/src/helpers/getTypeOf.js
+++ packages/reshow-runtime/src/helpers/getTypeOf.js
@@ -5,22 +5,28 @@ const types = '|number|string|boolean|object|function|undefined|';
  * The class name not reliable after code compress.
  */
 
-const getTypeOf = (val, name) => {
-  try {
-    const type = Object.getPrototypeOf(val).constructor.name.toLowerCase();
-    if (-1 === types.indexOf('|'+type+'|')) {
-      if (!name) {
-        name = type;
-      }
-      return name;
-    } else {
-      return type;
+const toBase = (type, name) => {
+  if (-1 === types.indexOf('|' + type + '|')) {
+    if (!name) {
+      name = type;
     }
+    return name;
+  } else {
+    return type;
+  }
+};
+
+const getTypeOf = (val, name) => {
+  let type;
+  try {
+    type = Object.getPrototypeOf(val).constructor.name.toLowerCase();
+    return toBase(type, name);
   } catch (ex) {
-    return Object.prototype.toString
+    type = Object.prototype.toString
       .call(val)
       .replace(/^\[object\s(.*)\]$/, '$1')
       .toLowerCase();
+    return toBase(type, name);
   }
 };
 
--- packages/reshow-runtime/tests/helpers/TypeofTest.js
+++ packages/reshow-runtime/tests/helpers/TypeofTest.js
@@ -2,27 +2,31 @@ import typeIs from '../../helpers/getTypeOf';
 import helperTypeof from '../../helpers/typeof';
 import {expect} from 'chai';
 
-describe('Test typeof', ()=>{ 
+describe('Test typeof', () => {
   class FOO {}
   class t {}
-  it('test typeIs', ()=>{
-      expect(typeIs(Symbol())).to.equal('symbol')
-      expect(typeIs({})).to.equal('object')
-      expect(typeIs(()=>{})).to.equal('function')
-      expect(typeIs(new FOO())).to.equal('foo')
-  })
-  it('test helper typeof', ()=>{
-      const symbol1 = Symbol();
-      const symbolTypeOf = helperTypeof(symbol1)
-      const objectTypeOf = helperTypeof({})
-      expect(symbolTypeOf).to.equal('symbol')
-      expect(objectTypeOf).to.equal('object')
-      expect(helperTypeof(new FOO())).to.equal('object')
-      expect(helperTypeof(new t())).to.equal('object')
-      expect(helperTypeof(1)).to.equal('number')
-      expect(helperTypeof('1')).to.equal('string')
-      expect(helperTypeof(true)).to.equal('boolean')
-      expect(helperTypeof(()=>{})).to.equal('function')
-      expect(helperTypeof(undefined)).to.equal('undefined')
-  })
-})
+  it('test typeIs', () => {
+    expect(typeIs(Symbol())).to.equal('symbol');
+    expect(typeIs({})).to.equal('object');
+    expect(typeIs(() => {})).to.equal('function');
+    expect(typeIs(new FOO())).to.equal('foo');
+  });
+  it('test helper typeof', () => {
+    const symbol1 = Symbol();
+    const symbolTypeOf = helperTypeof(symbol1);
+    const objectTypeOf = helperTypeof({});
+    expect(symbolTypeOf).to.equal('symbol');
+    expect(objectTypeOf).to.equal('object');
+    expect(helperTypeof(new FOO())).to.equal('object');
+    expect(helperTypeof(new t())).to.equal('object');
+    expect(helperTypeof(1)).to.equal('number');
+    expect(helperTypeof('1')).to.equal('string');
+    expect(helperTypeof(true)).to.equal('boolean');
+    expect(helperTypeof(() => {})).to.equal('function');
+    expect(helperTypeof(undefined)).to.equal('undefined');
+  });
+  it('test typeof array', () => {
+    expect(helperTypeof([])).to.equal('object');
+    expect(typeIs([])).to.equal('array');
+  });
+});
