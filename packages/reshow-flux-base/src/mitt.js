f477e890543b632ec3ee11913debe5a139c1d658
Hill <hill@kimo.com>
2018-11-29 12:52:39 +0800

    fixed emit not trigger

--- packages/reshow-flux-base/src/mitt.js
+++ packages/reshow-flux-base/src/mitt.js
@@ -4,11 +4,9 @@ const mitt = () => {
   const all = type({});
   return {
     on: (t, handler) => all(t).push(handler),
-
     off: (t, handler) => all(t).splice(all(t).indexOf(handler) >>> 0, 1),
-
     emit: (t, state) => {
-      for (let a = all(t), i = 0, j = a.length; i < j; i++) {
+      for (let a = all(t).slice(), i = 0, j = a.length; i < j; i++) {
         a[i](state);
       }
     },
