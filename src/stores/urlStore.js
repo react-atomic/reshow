c7037d2baeb3d729fa4fc4fc68e682ac4ffed30d
Hill <hill@kimo.com>
2019-02-12 20:10:14 +0000

    improve urlStore for server site rendering

--- src/stores/urlStore.js
+++ src/stores/urlStore.js
@@ -1,95 +1,97 @@
-import {ReduceStore} from 'reshow-flux'
-import urlDispatcher, {urlDispatch} from '../urlDispatcher'
-import get from 'get-object-value'
-import setUrl, {getUrl} from 'seturl'
+import {ReduceStore} from 'reshow-flux';
+import urlDispatcher, {urlDispatch} from '../urlDispatcher';
+import get from 'get-object-value';
+import setUrl, {getUrl} from 'seturl';
+import {win, doc} from 'win-doc';
 
-const keys = Object.keys
+const keys = Object.keys;
 
-const updateUrl = url => history.pushState('','',url)
+const updateUrl = url => history.pushState('', '', url);
 
-class URL 
-{
-    loc = {}
-    constructor(loc)
-    {
-        this.loc = {...loc}
+class URL {
+  loc = {};
+  constructor(loc) {
+    this.loc = {...loc};
+  }
+
+  getHref(loc) {
+    return this.loc.href;
+  }
+
+  get(key) {
+    let value;
+    if (0 === key.indexOf(':')) {
+      const cookKey = key.substr(1);
+      value = get(this.loc, [key.substr(1)]);
+      if ('pathname' === cookKey) {
+        value = value.split('/');
+      }
+    } else {
+      const href = this.getHref();
+      if (href) {
+        value = getUrl(key, this.getHref());
+      }
     }
-
-    getHref(loc)
-    {
-        return this.loc.href
-    }
-
-    get(key)
-    {
-        if (0 === key.indexOf(':')) {
-            const cookKey = key.substr(1)
-            const value = get(this.loc, [key.substr(1)])
-            if ('pathname' === cookKey) {
-                return value.split('/')
-            } else {
-                return value
-            }
-        } else {
-            return getUrl(key, this.getHref())
-        }
-    }
+    return value;
+  }
 }
 
-class UrlStore extends ReduceStore
-{
-    getInitialState()
-    {
-        let loc = {}
-        if ('undefined' !== typeof document) {
-            const doc = document
-            loc = doc.location
-            this.registerEvent(window)
-        }
-        return new URL(loc)
-    }
+class UrlStore extends ReduceStore {
+  getInitialState() {
+    let loc = {};
+    setTimeout(() => {
+      const oDoc = doc();
+      if (oDoc) {
+        urlDispatch({type: 'url', url: oDoc.URL});
+        this.registerEvent(win());
+      }
+    });
+    return new URL({});
+  }
 
-    registerEvent(win)
-    {
-        window.addEventListener("popstate", () => {
-            const doc = document
-            urlDispatch({type: 'url', url: doc.URL}) 
-        }, true);
+  registerEvent(win) {
+    if (win) {
+      win.addEventListener(
+        'popstate',
+        () => {
+          urlDispatch({type: 'url', url: doc().URL});
+        },
+        true,
+      );
     }
+  }
 
-    reduce (state, action)
-    {
-        if ('undefined' === typeof document) {
-            return state 
-        }
-        const doc = document
-        let url;
-        switch(action.type) {
-            case 'url':
-                url = get(action, ['url'])
-                if (!url) {
-                    console.error('Not assign url', action)
-                }
-                break
-            case 'query':
-                url = doc.URL
-                keys(get(action.params, null, [])).forEach( key => {
-                    url = setUrl(key, get(action, ['params', key]), url)            
-                })
-                break
-        }
-        if (url !== doc.URL) {
-            updateUrl(url)
-            const loc = doc.location
-            return new URL(loc)
-        } else {
-            if (url !== state.getHref()) {
-                const loc = doc.location
-                return new URL(loc)
-            }
-            return state
-        }
+  reduce(state, action) {
+    const oDoc = doc();
+    if (!oDoc) {
+      return state;
     }
+    const oLoc = new URL(oDoc.location);
+    let url;
+    switch (action.type) {
+      case 'url':
+        url = get(action, ['url']);
+        if (!url) {
+          console.error('Not assign url', action);
+        }
+        break;
+      case 'query':
+        url = oDoc.URL;
+        keys(get(action.params, null, [])).forEach(key => {
+          url = setUrl(key, get(action, ['params', key]), url);
+        });
+        break;
+    }
+    if (url !== oDoc.URL) {
+      updateUrl(url);
+      return oLoc;
+    } else {
+      if (url !== state.getHref()) {
+        return oLoc;
+      }
+      return state;
+    }
+  }
 }
 
-export default new UrlStore(urlDispatcher)
+export default new UrlStore(urlDispatcher);
