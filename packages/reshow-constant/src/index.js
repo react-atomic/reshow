6ebfaa85bccd75caa5585cb29e5ddd255ae5f8c1
Hill <hill@kimo.com>
2019-03-21 17:18:07 +0800

    add script constant

--- packages/reshow-constant/src/index.js
+++ packages/reshow-constant/src/index.js
@@ -1,6 +1,7 @@
 export const UNDEFINED = 'undefined';
 export const FUNCTION = 'function';
 export const OBJECT = 'object';
-export const SYMBOL = 'symbol';
 export const DEFAULT = 'default';
 export const STRING = 'string';
+export const SYMBOL = 'symbol';
+export const SCRIPT = 'script';
