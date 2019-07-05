83a7c8a3441e0cc36735b728650076d5c59198d6
Hill <hill@kimo.com>
2019-07-05 13:28:51 +0800

    Add objectSpread2
    https://github.com/react-atomic/reshow/issues/12

---
+++ packages/reshow-runtime/src/helpers/objectSpread2.js
@@ -0,0 +1,25 @@
+import defineProperty from "./defineProperty";
+import typeIs from './getTypeOf';
+import {FUNCTION} from 'reshow-constant';
+export default function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    if (i % 2 || !Object.getOwnPropertyDescriptors) {
+      var source = arguments[i] != null ? arguments[i] : {};
+      var ownKeys = Object.keys(source);
+
+      if (typeIs(Object.getOwnPropertySymbols) === FUNCTION) {
+        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        }));
+      }
+
+      ownKeys.forEach(function (key) {
+        defineProperty(target, key, source[key]);
+      });
+    } else {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(arguments[i]));
+    }
+  }
+
+  return target;
+}
