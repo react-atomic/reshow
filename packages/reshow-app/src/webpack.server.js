47af94d4dd9947497aa114ced76528785073c8b4
Hill <hill@kimo.com>
2019-08-05 15:31:36 +0800

    fixed tls support

--- packages/reshow-app/src/webpack.server.js
+++ packages/reshow-app/src/webpack.server.js
@@ -3,107 +3,101 @@ import webpack from 'webpack';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 
 const {
-    OccurrenceOrderPlugin,
-    ModuleConcatenationPlugin,
-    LimitChunkCountPlugin
+  OccurrenceOrderPlugin,
+  ModuleConcatenationPlugin,
+  LimitChunkCountPlugin,
 } = webpack.optimize;
-const {
-    NODE_ENV,
-    CONFIG
-} = process.env;
+const {NODE_ENV, CONFIG} = process.env;
 let confs = {};
 if (CONFIG) {
-    confs = JSON.parse(CONFIG);
+  confs = JSON.parse(CONFIG);
 }
 
-let plugins = [
-    new LimitChunkCountPlugin({maxChunks:1}),
-];
+let plugins = [new LimitChunkCountPlugin({maxChunks: 1})];
 let babelLoaderOption = {
-    cacheDirectory: true,
-    plugins: [
-        '@babel/plugin-syntax-dynamic-import'
-    ]
+  cacheDirectory: true,
+  plugins: ['@babel/plugin-syntax-dynamic-import'],
 };
 
 /* Default uglifyJs options */
 const uglifyJsOptions = {
-    cache: true,
-    parallel: true, 
-    uglifyOptions: {
-        compress: { 
-            unused: true,
-            dead_code: true,
-            join_vars: false,
-            hoist_funs: true,
-            collapse_vars: true,
-            passes:2,
-            side_effects: true,
-            warnings: false,
-        },
-        mangle: false,
-        output: {
-            comments: false,
-            beautify: false,
-        },
-    }
+  cache: true,
+  parallel: true,
+  uglifyOptions: {
+    compress: {
+      unused: true,
+      dead_code: true,
+      join_vars: false,
+      hoist_funs: true,
+      collapse_vars: true,
+      passes: 2,
+      side_effects: true,
+      warnings: false,
+    },
+    mangle: false,
+    output: {
+      comments: false,
+      beautify: false,
+    },
+  },
 };
 
-const myWebpack = (root, entry, lazyConfs)=>
-{
-    confs = {...confs, ...lazyConfs};
-    if ('production' === NODE_ENV) {
-        babelLoaderOption.envName = 'production';
-        plugins = plugins.concat([
-            new webpack.DefinePlugin({
-              'process.env':{
-                'NODE_ENV': JSON.stringify('production'),
-                '__DEVTOOLS__': false
-              }
-            }),
-            new ModuleConcatenationPlugin(),
-            new webpack.LoaderOptionsPlugin({
-                minimize: true,
-            }),
-            new UglifyJsPlugin(uglifyJsOptions),
-            new OccurrenceOrderPlugin(),
-        ]);
-    }
-    if (!entry) {
-        entry = {
-            node: './build/src/server.js'
-        };
-    }
-    return {
-        entry,
-        output: {
-            filename: "[name].bundle.js",
-            path: root + "/assets"
+const myWebpack = (root, entry, lazyConfs) => {
+  confs = {...confs, ...lazyConfs};
+  if ('production' === NODE_ENV) {
+    babelLoaderOption.envName = 'production';
+    plugins = plugins.concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+          __DEVTOOLS__: false,
         },
-        node: {
-            fs: "empty",
-            setImmediate: false
-        },
-        externals: confs.externals,
-        resolve: {
-            extensions: ['.mjs', '.js','.jsx'],
-            alias: confs.alias
-        },
-        resolveLoader: {
-            modules: [root + '/node_modules']
-        },
-        module: {
-            loaders: [
-                  { 
-                    test: /(.js|.jsx)$/, 
-                    exclude: /node_modules/,
-                    loader: "babel-loader", 
-                    options: babelLoaderOption 
-                  }
-            ]
-        },
-        plugins: plugins
+      }),
+      new ModuleConcatenationPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
+      new UglifyJsPlugin(uglifyJsOptions),
+      new OccurrenceOrderPlugin(),
+    ]);
+  }
+  if (!entry) {
+    entry = {
+      node: './build/src/server.js',
     };
+  }
+  return {
+    entry,
+    output: {
+      filename: '[name].bundle.js',
+      path: root + '/assets',
+    },
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      setImmediate: false,
+    },
+    externals: confs.externals,
+    resolve: {
+      extensions: ['.mjs', '.js', '.jsx'],
+      alias: confs.alias,
+    },
+    resolveLoader: {
+      modules: [root + '/node_modules'],
+    },
+    module: {
+      loaders: [
+        {
+          test: /(.js|.jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: babelLoaderOption,
+        },
+      ],
+    },
+    plugins: plugins,
+  };
 };
 
-module.exports=myWebpack;
+module.exports = myWebpack;
