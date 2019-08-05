62b0b0f30f9d3b356a4f3099811712ecdd45fd1b
Hill <hill@kimo.com>
2019-08-05 15:10:30 +0800

    fixed Buffer() deprecated warn
    Buffer() is deprecated due to security and usability issues.
    Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from()
    methods instead.

--- packages/reshow-app/src/server.js
+++ packages/reshow-app/src/server.js
@@ -2,32 +2,29 @@ const React = require('react');
 const ReactServer = require('react-dom/server');
 global.d3 = {}; // hack for ignore in d3 in server side
 
-const server = (app)=>
-{
-    const myApp = React.createFactory(app);
-    return ({process, fs, JSON, Buffer})=>{
-        process.env.node_env = 'production';
-        const fd = process.stdin.fd;
-        const bSize = 4096;
-        const buffer = new Buffer(bSize);
-        let temp = fs.readSync(fd, buffer, 0, bSize);
-        let context='';
-        while(temp){
-            context += buffer.toString('utf-8', 0, temp);
-            temp  = fs.readSync(fd, buffer, 0, bSize);
-        }
-        const myJson = JSON.parse(context);
-        const result = ReactServer.renderToString(
-            myApp( myJson )
-        );
-        const len = result.length;
-        process.stdout.write('<!--start-->');
-        let last=0;
-        while(last < len){
-            process.stdout.write(result.substr(last,1000));
-            last+=1000;
-        }
-    };
+const server = app => {
+  const myApp = React.createFactory(app);
+  return ({process, fs, JSON, Buffer}) => {
+    process.env.node_env = 'production';
+    const fd = process.stdin.fd;
+    const bSize = 4096;
+    const buffer = Buffer.alloc(bSize);
+    let temp = fs.readSync(fd, buffer, 0, bSize);
+    let context = '';
+    while (temp) {
+      context += buffer.toString('utf-8', 0, temp);
+      temp = fs.readSync(fd, buffer, 0, bSize);
+    }
+    const myJson = JSON.parse(context);
+    const result = ReactServer.renderToString(myApp(myJson));
+    const len = result.length;
+    process.stdout.write('<!--start-->');
+    let last = 0;
+    while (last < len) {
+      process.stdout.write(result.substr(last, 1000));
+      last += 1000;
+    }
+  };
 };
 
 module.exports = server;
