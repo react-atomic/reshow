7f0a4c2386d98a2498aebf6818b4f47e67275215
Hill <hill@kimo.com>
2020-01-03 17:12:33 +0800

    Add old babel-runtime support

--- packages/reshow-app/src/reshowRuntimeAlias.js
+++ packages/reshow-app/src/reshowRuntimeAlias.js
@@ -1,6 +1,9 @@
+const reshowRuntimePath = root + '/node_modules/reshow-runtime';
+
 const config = root => ({
-    '@babel/runtime/helpers/esm': root + '/node_modules/reshow-runtime/es',
-    '@babel/runtime': root + '/node_modules/reshow-runtime',
+  '@babel/runtime/helpers/esm': reshowRuntimePath + '/es',
+  '@babel/runtime': reshowRuntimePath,
+  'babel-runtime': reshowRuntimePath,
 });
 
 export default config;
