0ffabb54bf5c0112eef4d5a5a2cf7eb5e854f5e6
Hill <hill@kimo.com>
2020-01-07 02:29:37 +0800

    simplify connectHook

--- packages/reshow-flux/src/connectHook.js
+++ packages/reshow-flux/src/connectHook.js
@@ -1,52 +1,28 @@
-import {useMemo, useState, useReducer, useEffect} from 'react';
+import {useMemo, useState, useEffect} from 'react';
 import build from 'reshow-build';
 import callfunc from 'call-func';
 import dedup from 'array.dedup';
 import {CHANGE} from 'reshow-flux-base';
 
-const setStores = (state, stores, handleChange, dispatch) => {
-  if (state && state.stores) {
-    resetStores(state);
-  }
-  if (stores && stores.length) {
-    stores = dedup(stores) || [];
-    stores.forEach(store => store.addListener(handleChange, CHANGE));
-  }
-  dispatch({stores, handleChange});
-};
-
-const resetStores = state => {
-  const {stores, handleChange} = state || {};
-  if (!stores || !stores.length) {
-    return;
-  }
-  stores.forEach(store => store.removeListener(handleChange, CHANGE));
-};
-
-const reducer = (state, action) => {
-  state = state || {};
-  state.stores = action.stores;
-  state.handleChange = action.handleChange;
-  return state;
-};
-
 const connectHook = (Base, options) => {
   const {calculateState, defaultProps, getStores} = options || {};
   const arrState = {};
   const Connected = props => {
-    const [state, dispatch] = useReducer(reducer);
     const [data, setData] = useState(() => calculateState({}, props));
     useEffect(() => {
+      const stores = dedup(getStores(props)) || [];
       const handleChange = () => {
         setData(prevState => ({
           ...prevState,
           ...calculateState(prevState, props),
         }));
       };
-      setStores(state, getStores(props), handleChange, dispatch);
-      return () => {
-        resetStores(state);
-      };
+      if (stores && stores.length) {
+        stores.forEach(store => store.addListener(handleChange, CHANGE));
+        return () => {
+          stores.forEach(store => store.removeListener(handleChange, CHANGE));
+        };
+      }
     });
     return useMemo(() => build(Base)({...props, ...data}), [props, data]);
   };
