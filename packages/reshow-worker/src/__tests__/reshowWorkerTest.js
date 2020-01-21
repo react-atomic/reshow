32627ee66f948625432c4db2ff27f21e4eaab391
Hill <hill@kimo.com>
2020-01-21 18:25:43 +0800

    Add lock ajax version

---
+++ packages/reshow-worker/src/__tests__/reshowWorkerTest.js
@@ -0,0 +1,17 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+import initWorker from '../index';
+
+describe('Test reshowWorker', () => {
+  it('test init', ()=>{
+    const win = {Worker: false};
+    initWorker(win);
+  });
+  it('test import is call', ()=>{
+    const win = {Worker: true};
+    const spy = sinon.spy();
+    initWorker(win, spy);
+    expect(spy.called).to.be.true;
+  });
+});
--- packages/reshow-worker/src/index.js
+++ packages/reshow-worker/src/index.js
@@ -1,20 +1,25 @@
 import {initAjaxWorkerEvent} from 'organism-react-ajax';
 import {getDefault} from 'get-object-value';
+import {win} from 'win-doc';
 
-const initWorker = () =>
-{
-    if ('undefined' !== typeof window) {
-        if (window.Worker) {
-            import('worker-loader!organism-react-ajax/build/es/src/worker').
-                then( workerObject => {
-                    workerObject = getDefault(workerObject);
-                    if (workerObject) {
-                        const objWorker = new workerObject();
-                        initAjaxWorkerEvent(objWorker);
-                    }
-            });
-        }
-    }
+const importWorker = () => {
+  import('worker-loader!organism-react-ajax/build/es/src/worker').then(
+    workerObject => {
+      workerObject = getDefault(workerObject);
+      if (workerObject) {
+        const objWorker = new workerObject();
+        initAjaxWorkerEvent(objWorker);
+      }
+    },
+  );
+};
+
+const initWorker = (oWin, cb) => {
+  oWin = oWin || win();
+  cb = cb || importWorker;
+  if (oWin.Worker) {
+    cb();
+  }
 };
 
 export default initWorker;
