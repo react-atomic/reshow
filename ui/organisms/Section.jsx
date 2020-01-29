0cdfd65a12d6ce9b24111b068566b54e3c9404ae
Hill <hill@kimo.com>
2020-01-30 03:04:23 +0800

    change section that pass name to children

--- ui/organisms/Section.jsx
+++ ui/organisms/Section.jsx
@@ -12,44 +12,39 @@ const myReturnOptions = {
   defaultProps: {
     ...otherDefaultProps,
     initStates: ['section', 'I18N'],
-  }
-}
+  },
+};
 
 const Section = props => {
-    const {
-      name,
-      section,
-      immutable,
-      children,
-      ...otherProps
-    } = props;
-    if (!section) {
+  const {section, immutable, children, ...otherProps} = props;
+  if (!section) {
+    return null;
+  }
+  const name = props.name;
+  let allParams = myReturnOptions.reset(otherProps);
+  if (immutable) {
+    const thisSection = section.get(name);
+    if (!thisSection) {
       return null;
     }
-    let allParams = myReturnOptions.reset(otherProps);
-    if (immutable) {
-      const thisSection = section.get(name);
-      if (!thisSection) {
-        return null;
-      }
-      const shouldRender = thisSection.get('shouldRender');
-      if (!shouldRender) {
-        return null;
-      }
-      thisSection
-        .delete('shouldRender')
-        .keySeq()
-        .forEach(key => {
-          allParams[key] = thisSection.get(key);
-        });
-    } else {
-      const {shouldRender, ...others} = get(section, [name], {});
-      if (!shouldRender) {
-        return null;
-      }
-      allParams = {...others, ...allParams};
+    const shouldRender = thisSection.get('shouldRender');
+    if (!shouldRender) {
+      return null;
     }
-    return build(children)(allParams);
+    thisSection
+      .delete('shouldRender')
+      .keySeq()
+      .forEach(key => {
+        allParams[key] = thisSection.get(key);
+      });
+  } else {
+    const {shouldRender, ...others} = get(section, [name], {});
+    if (!shouldRender) {
+      return null;
+    }
+    allParams = {...others, ...allParams};
+  }
+  return build(children)(allParams);
 };
 
 Section.displayName = 'Section';
