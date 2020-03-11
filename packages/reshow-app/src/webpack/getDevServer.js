b0a7f6504e1d5d569cc7021395234d2e6cc61870
Hill <hill@omnibpm.com>
2020-03-11 16:13:14 +0800

    add remote dev server support

--- packages/reshow-app/src/webpack/getDevServer.js
+++ packages/reshow-app/src/webpack/getDevServer.js
@@ -1,11 +1,11 @@
-const getDevServer = ({confs, path}) => {
-  return {
-    port: confs.devPort || 8080, 
-    publicPath: confs.assetsRoot,
-    writeToDisk: true,
-    liveReload: false,
-    hot: true,
-  };
-}
+const getDevServer = ({confs, path}) => ({
+  host: '0.0.0.0',
+  disableHostCheck: true,
+  port: confs.devPort || 8080,
+  publicPath: confs.assetsRoot,
+  writeToDisk: true,
+  liveReload: false,
+  hot: true,
+});
 
 export default getDevServer;
