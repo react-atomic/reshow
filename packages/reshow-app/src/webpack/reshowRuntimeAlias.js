8cd21237da0d670a6dbcc6fd0e6b300923150d7c
Hill <hill@kimo.com>
2020-07-24 18:08:22 +0800

    fixed babel runtime esm

--- packages/reshow-app/src/webpack/reshowRuntimeAlias.js
+++ packages/reshow-app/src/webpack/reshowRuntimeAlias.js
@@ -1,7 +1,7 @@
 const config = root => {
   const reshowRuntimePath = root + '/node_modules/reshow-runtime';
   return {
-    '@babel/runtime/helpers/esm': reshowRuntimePath + '/es',
+    '@babel/runtime/helpers/esm': reshowRuntimePath + '/es/helpers',
     '@babel/runtime/helpers': reshowRuntimePath + '/helpers',
     'babel-runtime/helpers': reshowRuntimePath + '/helpers',
   };
