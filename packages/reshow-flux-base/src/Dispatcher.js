640af9dc27772684f3583cdfdc0f1f20790ff9d1
Hill <hill@kimo.com>
2020-08-10 09:04:47 +0800

    Add nested dispatch warning

--- packages/reshow-flux-base/src/Dispatcher.js
+++ packages/reshow-flux-base/src/Dispatcher.js
@@ -1,17 +1,32 @@
 import "setimmediate";
 import { STRING } from "reshow-constant";
+import callfunc from "call-func";
 
 const Dispatcher = () => {
+  let isRunning = false;
   const cbs = [];
   const register = (cb) => cbs.push(cb);
 
-  const dispatch = (payload, params) => {
+  const dispatch = (payload, params, callback) => {
     payload = payload || {};
     if (STRING === typeof payload) {
       payload = { type: payload, params };
       !params && delete payload.params;
     }
-    setImmediate(() => cbs.forEach((c) => c(payload)));
+    if (isRunning) {
+      setImmediate(() => {
+        cbs.forEach((c) => c(payload));
+        callfunc(callback, [isRunning]);
+      });
+      console.warn("Should avoid nested dispath");
+    } else {
+      isRunning = true;
+      setImmediate(() => {
+        cbs.forEach((c) => c(payload));
+        isRunning = false;
+        callfunc(callback, [isRunning]);
+      });
+    }
   };
   return { register, dispatch };
 };
