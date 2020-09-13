dd638c3a5af42e7e11bb4941eef788c91695ed2e
Hill <hill@kimo.com>
2020-09-13 17:45:08 +0800

    improve NginxPush

--- packages/reshow-app/src/webpack/getPlugins.js
+++ packages/reshow-app/src/webpack/getPlugins.js
@@ -1,40 +1,49 @@
-import webpack from 'webpack';
-import Refresh from './refresh';
-import getStatsJson from './getStatsJson';
-import getBundleAnalyzerPlugin from './getBundleAnalyzerPlugin';
-import FinishPlugin from './FinishPlugin';
-import {PRODUCTION} from './const';
+import webpack from "webpack";
+import Refresh from "./refresh";
+import getStatsJson from "./getStatsJson";
+import getBundleAnalyzerPlugin from "./getBundleAnalyzerPlugin";
+import FinishPlugin from "./FinishPlugin";
+import NginxPushPlugin from "./NginxPushPlugin";
+import { PRODUCTION } from "./const";
 
-const {AggressiveMergingPlugin, LimitChunkCountPlugin} = webpack.optimize;
+const { AggressiveMergingPlugin, LimitChunkCountPlugin } = webpack.optimize;
 
-const getPlugins = ({path, stop, mode, confs, BUNDLE, HOT_UPDATE, server}) => {
-  const plugins = [getStatsJson()];
+const getPlugins = ({
+  path,
+  stop,
+  mode,
+  confs,
+  BUNDLE,
+  HOT_UPDATE,
+  server,
+}) => {
+  const plugins = [getStatsJson(), new NginxPushPlugin(confs)];
   let maxChunks = confs.maxChunks;
   if (server) {
     maxChunks = 1;
   }
   if (maxChunks != null) {
-    plugins.push(new LimitChunkCountPlugin({maxChunks}));
+    plugins.push(new LimitChunkCountPlugin({ maxChunks }));
   }
   if (BUNDLE) {
-    plugins.push(getBundleAnalyzerPlugin({BUNDLE}));
+    plugins.push(getBundleAnalyzerPlugin({ BUNDLE }));
   }
   if (mode === PRODUCTION) {
     plugins.push(
       new AggressiveMergingPlugin({
         minSizeReduce: 2,
         moveToParents: true,
-      }),
+      })
     );
   }
   if (HOT_UPDATE) {
     plugins.push(
       new webpack.HotModuleReplacementPlugin(),
-      new Refresh({disableRefreshCheck: true}),
+      new Refresh({ disableRefreshCheck: true })
     );
   }
   if (stop) {
-    plugins.push(new FinishPlugin({stop}));
+    plugins.push(new FinishPlugin({ stop }));
   }
   return plugins;
 };
