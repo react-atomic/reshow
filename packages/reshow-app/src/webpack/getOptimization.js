fb9e9cc798a55681169e2e48159c07b17258f0d6
Hill <hill@kimo.com>
2020-10-29 13:45:12 +0800

    update reshow app NginxPushPlugin

--- packages/reshow-app/src/webpack/getOptimization.js
+++ packages/reshow-app/src/webpack/getOptimization.js
@@ -3,14 +3,7 @@ import getTerser from "./getTerser";
 import { PRODUCTION } from "./const";
 
 const getOptimization = ({ mode, server, confs }) => {
-  const cacheGroups = {
-    commons: {
-      chunks: "initial",
-      minChunks: 2,
-      maxInitialRequests: 5, // The default limit is too small to showcase the effect
-      minSize: 0, // This is example is too small to create commons chunks
-    },
-  };
+  const cacheGroups = { };
   if (!confs.disableVendor) {
     cacheGroups.vendor = getVendorSplitConfig({ confs });
   }
--- packages/reshow-app/src/webpack/getVendor.js
+++ packages/reshow-app/src/webpack/getVendor.js
@@ -1,12 +1,17 @@
+/**
+ * https://webpack.js.org/plugins/split-chunks-plugin/#splitchunkschunks
+ *
+ */
 const getVendorSplitConfig = ({ confs }) => ({
   test: /node_modules/,
-  // https://webpack.js.org/plugins/split-chunks-plugin/#splitchunkschunks
   chunks: "initial",
   name: "vendor",
   filename:
     confs.bustMode === "name" ? "[name].[hash].bundle.js" : "[name].bundle.js",
   priority: 10,
   enforce: true,
+  reuseExistingChunk: true,
+  minChunks: 2,
 });
 
 export { getVendorSplitConfig };
