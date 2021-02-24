544ec7750cdca9c3d9c25bba5e2c34ea9f081bb2
Hill <hill@kimo.com>
2021-02-24 14:23:23 +0800

    fixed node

--- packages/reshow-app/src/webpack/getEntry.js
+++ packages/reshow-app/src/webpack/getEntry.js
@@ -2,7 +2,7 @@ const getEntry = ({ main, confs, server }) => {
   let entry;
   if (!main) {
     if (server) {
-      entry = { main: "./build/src/server.js" };
+      entry = { node: "./build/src/server.js" };
     } else {
       entry = { main: "./build/src/client.js" };
     }
