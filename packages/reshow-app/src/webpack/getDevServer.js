3b37fb0af7aa5dad8319e140b4dd29ea7c7ea4c3
Hill <hill@kimo.com>
2021-03-18 02:43:08 +0800

    rename devPort to hotPort

--- packages/reshow-app/src/webpack/getDevServer.js
+++ packages/reshow-app/src/webpack/getDevServer.js
@@ -1,7 +1,7 @@
 const getDevServer = ({ confs, path }) => ({
   host: "0.0.0.0",
   disableHostCheck: true,
-  port: confs.devPort || 8080,
+  port: confs.hotPort ?? 8080,
   publicPath: confs.assetsRoot,
   writeToDisk: true,
   liveReload: false,
