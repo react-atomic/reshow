0c5883c82638d6a5009109880df621f68d467ab3
Hill <hill@kimo.com>
2021-04-16 18:43:11 +0000

    update script

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ develop(){
 }
 
 killBy(){
-    ps auxwwww | grep $1 | grep -v grep | awk '{print $2}' | xargs -I{} kill -9 {}
+    ps -eo pid,args | grep $1 | grep -v grep | awk '{print $1}' | xargs -I{} kill -9 {}
 }
 
 stop(){
