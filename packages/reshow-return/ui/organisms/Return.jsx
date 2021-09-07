7183a5ba89005b79997cd6da0f80bc9d7b2d25ac
Hill <hill@kimo.com>
2021-09-08 06:55:45 +0800

    prepare react 17

--- packages/reshow-return/ui/organisms/Return.jsx
+++ packages/reshow-return/ui/organisms/Return.jsx
@@ -4,9 +4,9 @@ import { useConnect } from "reshow-flux";
 
 import returnOptions from "../../src/returnOptions";
 
-const getReturn = ({ defaultProps, displayName = "Return" }) => {
+const getReturn = ({ useConnect, displayName = "Return" }) => {
   const Return = (props) => {
-    const { useConnect, children, cleanProps, ...otherProps } = props;
+    const { children, cleanProps, ...otherProps } = props;
     const state = useConnect(props);
     const result = build(children)({
       ...returnOptions.reset(otherProps, cleanProps),
@@ -15,13 +15,12 @@ const getReturn = ({ defaultProps, displayName = "Return" }) => {
     return result;
   };
 
-  Return.defaultProps = defaultProps;
   Return.displayName = displayName;
   return Return;
 };
 
 const Return = getReturn({
-  defaultProps: { useConnect: useConnect(returnOptions) },
+  useConnect: useConnect(returnOptions),
 });
 
 export default Return;
