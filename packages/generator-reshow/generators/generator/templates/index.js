4ed9b2cbb51a1908d031722ae4c7c1137fc40e98
Hill <hill@kimo.com>
2022-01-01 02:03:15 +0800

    update generator tmpl

--- packages/generator-reshow/generators/generator/templates/index.js
+++ packages/generator-reshow/generators/generator/templates/index.js
@@ -1,15 +1,10 @@
 const getYo = require("yo-reshow");
-const { YoGenerator, YoHelper } = getYo();
+const { YoGenerator, YoHelper, commonPrompt } = getYo();
 
 /**
  * <%= mainName %> Generator
  */
 module.exports = class extends YoGenerator {
-  constructor(args, opts) {
-    super(args, opts);
-    this.argument("mainName", { type: String, required: false });
-  }
-
   /**
    * Run loop (Life cycle)
    * https://yeoman.io/authoring/running-context.html#the-run-loop
@@ -31,82 +26,40 @@ module.exports = class extends YoGenerator {
    * https://github.com/SBoudrias/Inquirer.js
    */
   async prompting() {
-    this.env.options.nodePackageManager = "yarn";
-
     const {
       say,
-      getDestFolderName,
+      handleAnswers,
       mergePromptOrOption,
       promptChainLocator,
       promptChain,
-      getAllAns,
     } = YoHelper(this);
-    const { mainName } = this.options;
-    let namePrompt = [];
-    if (!mainName) {
-      say(
-        'Generate "<%= mainName %>"\n\n !! \n\nYou need create folder\n by yourself.'
-      );
-      namePrompt = [
-        {
-          type: "confirm",
-          name: "isReady",
-          message: `We will put files at [${getDestFolderName()}], do you confirm it?`,
-          default: false,
-        },
-        {
-          when: (response) => {
-            if (!getAllAns().isReady) {
-              say("Exit for not ready to create folder.");
-              process.exit(0);
-            }
-          },
-        },
-      ];
-    }
 
     const prompts = [
-      ...namePrompt,
+      ...commonPrompt.mainName(this),
+      ...commonPrompt.desc(this),
+      /*
       {
         type: "input",
-        name: "mainName",
-        message: "Please input your <%= mainName %> name?",
-        default: mainName || getDestFolderName(),
-      },
-      {
-        type: "input",
-        name: "description",
-        message:
-          "Please input description for <%= mainName %>?",
+        name: "xxx",
+        message: "Please input xxx?",
         default: "",
       },
-      {
-        type: "input",
-        name: "keyword",
-        message: "Please input keyword for <%= mainName %>?",
-        default: "",
-      },
+      */
     ];
 
-    const answers =  await mergePromptOrOption(
-      prompts,
-      (nextPrompts) => promptChain(promptChainLocator(nextPrompts))
+    const answers = await mergePromptOrOption(prompts, (nextPrompts) =>
+      promptChain(promptChainLocator(nextPrompts))
     );
-
-    say(answers);
-
-    this.mainName = answers.mainName;
-    this.payload = {
-      mainName: this.mainName,
-      description: answers.description || 'TODO: description',
-      keyword: answers.keyword || this.mainName,
-    };
+    handleAnswers(answers, payload => { say(payload) });
   }
 
   writing() {
-    const { cp, chdir, getDestFolderName } = YoHelper(this);
-    if (this.mainName !== getDestFolderName()) {
-      chdir(this.mainName);
-    }
+    this.env.options.nodePackageManager = "yarn";
+    const { cp,  chMainName } = YoHelper(this);
+
+    // handle change to new folder
+    chMainName(this.mainName);
+
+    // handle copy file
   }
 };
