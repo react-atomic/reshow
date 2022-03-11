9554495933d434234603a2e854340f3c5ab6ca5f
Hill <hill@kimo.com>
2022-03-11 22:14:19 +0800

    update terser

--- packages/reshow-app/src/webpack/getTerser.js
+++ packages/reshow-app/src/webpack/getTerser.js
@@ -1,7 +1,7 @@
 import TerserPlugin from "terser-webpack-plugin";
 
 /**
- * Doc
+ * terser-webpack-plugin Doc
  * https://github.com/webpack-contrib/terser-webpack-plugin
  *
  * terserOptions
@@ -15,22 +15,20 @@ const getTerser = () => {
   const terser = new TerserPlugin({
     parallel: true,
     terserOptions: {
-      cache: true,
-      beautify: false,
       ecma: 5,
-      warnings: false,
       parse: {},
       compress: {
         passes: 2,
+        keep_classnames: true,
+        toplevel: true,
       },
       mangle: true,
-      module: false,
-      output: {
+      format: {
         comments: false,
       },
+      keep_classnames: true,
       toplevel: true,
       nameCache: null,
-      keep_classnames: true,
       ie8: true,
       safari10: true,
     },
