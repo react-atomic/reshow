091b9fec8fda95d8c438dda493f75eb5a9ab1b51
Hill <hill@kimo.com>
2022-05-05 13:18:49 +0800

    improve code

--- packages/reshow-hooks/src/useLoaded.js
+++ packages/reshow-hooks/src/useLoaded.js
@@ -1,11 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import callfunc from "call-func";
+import { T_FALSE, T_TRUE } from "reshow-constant";
 
 const useLoaded = (onChange) => {
-  const [isLoad, setIsLoad] = useState(() => callfunc(onChange, [false]) ?? false);
+  const [isLoad, setIsLoad] = useReducer(
+    () => T_TRUE,
+    T_FALSE,
+    (bool) => callfunc(onChange, [bool]) ?? bool
+  );
   useEffect(() => {
-    setIsLoad(true);
-    callfunc(onChange, [true]);
+    setIsLoad();
+    callfunc(onChange, [T_TRUE]);
   }, []);
   return isLoad;
 };
