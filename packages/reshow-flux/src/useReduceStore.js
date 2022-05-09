1f79963fd7d60b2661a95dc41c0250c36270f096
Hill <hill@kimo.com>
2022-05-09 08:12:19 +0800

    improve useReduceStore

--- packages/reshow-flux/src/useReduceStore.js
+++ packages/reshow-flux/src/useReduceStore.js
@@ -3,10 +3,10 @@ import { createReducer } from "reshow-flux-base";
 import { Map, defaultReducer } from "./ImmutableStore";
 
 const useReduceStore = (reduce, initState) => {
-  reduce = reduce || defaultReducer;
-  initState = initState || Map();
   const lastReducer = useRef();
   if (!lastReducer.current) {
+    reduce = reduce || defaultReducer;
+    initState = initState || Map();
     lastReducer.current = createReducer(reduce, initState);
   }
   return lastReducer.current;
