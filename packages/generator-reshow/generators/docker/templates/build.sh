4d997c4fe5ecbe23117afb47b2782f42b3a2f4a9
Hill <hill@kimo.com>
2022-05-20 21:50:33 +0800

    improve docker template

--- packages/generator-reshow/generators/docker/templates/build.sh
+++ packages/generator-reshow/generators/docker/templates/build.sh
@@ -6,17 +6,22 @@ DIR="$(
 )"
 
 FOLDER_PREFIX=$(${DIR}/support/FOLDER_PREFIX.sh)
+BUILD_VERSION=$1
+
+if [ -z "$BUILD_VERSION" ]; then
+  echo "Not set build version."
+  exit 1;
+fi
 
 do_build() {
-  SED_REPLACE_VER=$1
-  DEST_FOLDER=${DIR}/${FOLDER_PREFIX}${SED_REPLACE_VER}
+  DEST_FOLDER=${DIR}/${FOLDER_PREFIX}${BUILD_VERSION}
   mkdir -p ${DEST_FOLDER}
-  echo "building --- Version: " $SED_REPLACE_VER "-->"
+  echo 'building --- Version: ' $BUILD_VERSION '-->'
   DEST_FILE=${DEST_FOLDER}/Dockerfile
   cp Dockerfile ${DEST_FILE}
   cp install-packages.sh ${DEST_FOLDER}
-  sed -i -e "s|\[VERSION\]|$SED_REPLACE_VER|g" ${DEST_FILE}
+  sed -i -e "s|\[VERSION\]|$BUILD_VERSION|g" ${DEST_FILE}
   if [ -e "${DEST_FILE}-e" ]; then rm ${DEST_FILE}-e; fi
 }
 
-do_build $1
+do_build
