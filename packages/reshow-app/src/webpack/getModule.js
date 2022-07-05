08c708f7d94a95c03da9ba144fa3213fa06c0958
Hill <hill@kimo.com>
2022-07-06 02:40:24 +0800

    change to use swc-loader

--- packages/reshow-app/src/webpack/getModule.js
+++ packages/reshow-app/src/webpack/getModule.js
@@ -1,21 +1,6 @@
 import { PRODUCTION } from "./const";
 
 const getModule = ({ mode, HOT_UPDATE }) => {
-  const babelLoaderOption = {
-    cacheDirectory: true,
-    plugins: ["@babel/plugin-syntax-dynamic-import"],
-    presets: [["@babel/preset-env", { modules: false }]],
-  };
-  if (mode === PRODUCTION) {
-    babelLoaderOption.envName = PRODUCTION;
-    babelLoaderOption.plugins.push(
-      // https://www.npmjs.com/package/babel-plugin-transform-react-remove-prop-types#options
-      ["transform-react-remove-prop-types", { mode: "remove" }]
-    );
-  }
-  if (HOT_UPDATE) {
-    babelLoaderOption.plugins.push("react-refresh/babel");
-  }
   const module = {
     rules: [
       {
@@ -25,15 +10,9 @@ const getModule = ({ mode, HOT_UPDATE }) => {
             type: "asset/source",
           },
           {
-            test: /(.js|.jsx)$/,
+            test: /\.m?js$/,
             exclude: [/node_modules/],
-            use: [
-              "thread-loader",
-              {
-                loader: "babel-loader",
-                options: babelLoaderOption,
-              },
-            ],
+            use: { loader: "swc-loader" },
           },
         ],
       },
@@ -43,3 +22,22 @@ const getModule = ({ mode, HOT_UPDATE }) => {
 };
 
 export default getModule;
+
+/**
+ * Babel usage
+const babelLoaderOption = {
+  cacheDirectory: true,
+  plugins: ["@babel/plugin-syntax-dynamic-import"],
+  presets: [["@babel/preset-env", { modules: false }]],
+};
+if (mode === PRODUCTION) {
+  babelLoaderOption.envName = PRODUCTION;
+  babelLoaderOption.plugins.push(
+    // https://www.npmjs.com/package/babel-plugin-transform-react-remove-prop-types#options
+    ["transform-react-remove-prop-types", { mode: "remove" }]
+  );
+}
+if (HOT_UPDATE) {
+  babelLoaderOption.plugins.push("react-refresh/babel");
+}
+*/
