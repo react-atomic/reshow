4d829889d72e543aeb5484e879bf5c22096e0fe7
Hill <hill@kimo.com>
2022-07-11 03:18:13 +0800

    mainNamePrompt add custom i18n

--- packages/yo-reshow/src/mainNamePrompt.js
+++ packages/yo-reshow/src/mainNamePrompt.js
@@ -1,28 +1,42 @@
+const callfunc = require("call-func");
 const YoHelper = require("./YoHelper");
 
-const mainNamePrompt = (oGen) => {
+const defaultI18n = {
+  tip: ({ namespace }) =>
+    `Generate "${namespace}"\n\n !! \n\nYou need create folder\n by yourself.`,
+  isReady: ({ destFolderName }) =>
+    `We will put files to [${destFolderName}], do you confirm it?`,
+  notReady: "Exit for not ready to create folder.",
+  mainName: "Please confirm your naming? Or rename it.",
+};
+
+const mainNamePrompt = (oGen, i18n) => {
   const { say, getDestFolderName, getAllAns, exit } = YoHelper(oGen);
+  i18n = {...defaultI18n, ...i18n};
+
+  /**
+   * How to use argument?
+   * @see https://yeoman.io/authoring/user-interactions.html#arguments 
+   */
   oGen.argument("argMainName", { type: String, required: false });
   const { argMainName, namespace } = oGen.options;
   oGen.options.subGeneratorName = namespace.split(":")[1];
 
   const prompts = [];
   if (!argMainName) {
-    say(
-      `Generate "${namespace}"\n\n !! \n\nYou need create folder\n by yourself.`
-    );
+    say(callfunc(i18n.tip, [{namespace}]));
     prompts.push(
       {
         type: "confirm",
         name: "isReady",
-        message: `We will put files at [${getDestFolderName()}], do you confirm it?`,
+        message: callfunc(i18n.isReady, [{destFolderName: getDestFolderName()}]),
         default: false,
       },
       {
         when: (response) => {
           const allAns = getAllAns(response);
           if (!allAns.isReady) {
-            exit(() => say("Exit for not ready to create folder."));
+            exit(() => say(callfunc(i18n.notReady)));
           }
         },
       }
@@ -31,7 +45,7 @@ const mainNamePrompt = (oGen) => {
   prompts.push({
     type: "input",
     name: "mainName",
-    message: "Please confirm your naming? Or rename it.",
+    message: callfunc(i18n.mainName),
     default: argMainName || getDestFolderName(),
   });
   return prompts;
