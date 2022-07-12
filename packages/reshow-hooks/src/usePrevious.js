bdfa07aab4cb696f858c806ba0955b49139d8388
Hill <hill@kimo.com>
2022-07-12 13:39:50 +0800

    add You Might Not Need an Effect

--- packages/reshow-hooks/src/usePrevious.js
+++ packages/reshow-hooks/src/usePrevious.js
@@ -1,5 +1,9 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * Check more info
+ * https://beta.reactjs.org/learn/you-might-not-need-an-effect?#adjusting-some-state-when-a-prop-changes
+ */
 const usePrevious = (value) => {
   const prevValue = useRef();
   useEffect(() => {
