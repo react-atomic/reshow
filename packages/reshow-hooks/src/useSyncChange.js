f94a7a5811c3bc1f9de3d17f1dc9437ee68b6dfb
Hill <hill@kimo.com>
2022-07-15 00:36:14 +0800

    add comparator

--- packages/reshow-hooks/src/useSyncChange.js
+++ packages/reshow-hooks/src/useSyncChange.js
@@ -1,8 +1,8 @@
 import usePrevious from "./usePrevious";
 import callfunc from "call-func";
 
-const useSyncChange = (value, setter, init) => {
-  if (value !== usePrevious(value, init)) {
+const useSyncChange = (value, setter, init, comparator = (a, b) => a !== b) => {
+  if (comparator(value, usePrevious(value, init))) {
     callfunc(setter, [value]);
   }
 };
