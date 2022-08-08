f8f66cbf01d0dca331d32fc898a249fa829176a4
Hill <hill@kimo.com>
2022-08-08 12:59:36 +0800

    renderKeys could use array like data structure.

--- packages/reshow-return/src/usePartialRender.js
+++ packages/reshow-return/src/usePartialRender.js
@@ -1,6 +1,5 @@
 import { useState, useMemo } from "react";
 import { useReduceStore, Map } from "reshow-flux";
-import { IS_ARRAY } from "reshow-constant";
 import build from "reshow-build";
 import callfunc from "call-func";
 
@@ -15,7 +14,12 @@ const usePartialRender = (initRenderKeys, initChildren) => {
 
   const renderItems = useMemo(() => {
     const buildReturn = build(build(Return)({ store: renderStore }));
-    return (IS_ARRAY(renderKeys) ? renderKeys : []).map((name) =>
+
+    /**
+     * renderKeys could use array like data structure.
+     * such as immutable.js
+     */
+    return (renderKeys && renderKeys.map ? renderKeys : []).map((name) =>
       buildReturn(
         { key: name, name, initStates: [name] },
         (props) => props[props.name] || null
