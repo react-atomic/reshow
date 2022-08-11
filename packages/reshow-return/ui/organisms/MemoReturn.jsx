04739830b97979ac24aac6222c2950dcf331c3d2
Hill <hill@kimo.com>
2022-08-11 18:06:42 +0800

    Add actualProps

--- packages/reshow-return/ui/organisms/MemoReturn.jsx
+++ packages/reshow-return/ui/organisms/MemoReturn.jsx
@@ -2,6 +2,9 @@ import { useMemo } from "react";
 import build from "reshow-build";
 
 const MemoReturn = (props) =>
-  useMemo(() => build(props.children)(props.props), [props.props]);
+  useMemo(
+    () => build(props.children)(props.actualProps || props.props),
+    [props.props]
+  );
 
 export default MemoReturn;
