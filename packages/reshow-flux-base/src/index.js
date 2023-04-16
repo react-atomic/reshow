7d5b73e0e6b2e1d0547106ecf8cbac5b25e0580f
Hill <hill@kimo.com>
2023-04-16 17:27:04 +0800

    change to export interface directly

--- packages/reshow-flux-base/src/index.js
+++ packages/reshow-flux-base/src/index.js
@@ -2,17 +2,13 @@
 
 /**
  * @template StateType
- * @typedef {import("./createReducer").StoreObject<StateType>} StoreObject
- */
-
-/**
- * @template StateType
  * @typedef {import("./createReducer").InitStateType<StateType>} InitStateType
  */
 
-/**
- * @typedef {import("./createReducer").ActionObject} ActionObject
- */
-
-export { default as createReducer, refineAction } from "./createReducer";
+export {
+  default as createReducer,
+  refineAction,
+  StoreObject,
+  ActionObject,
+} from "./createReducer";
 export { default as SimpleMap } from "./SimpleMap";
